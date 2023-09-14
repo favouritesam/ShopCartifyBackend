d9c5914e3566d8f2fc7457aa03f033ebcf62c79b
LordNedStark <osisiogubenjamin1@gmail.com>
2023-09-14 10:34:11 +0100

    first commit

---
+++ src/main/java/caritfy/shop/shopcaritfy/ShopCaritfyApplication.java
@@ -0,0 +1,13 @@
+package caritfy.shop.shopcaritfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCaritfyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopCaritfyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/caritfy/shop/shopcaritfy/ShopCaritfyApplicationTests.java
@@ -0,0 +1,13 @@
+package caritfy.shop.shopcaritfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCaritfyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
